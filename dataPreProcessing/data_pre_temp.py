dd21613ec72ea858141d2f366921747efca36bcb
Abhinav Malhotra <113239170+abhinavmalhotra01@users.noreply.github.com>
2022-12-26 12:59:37 +0530

    Add files via upload

---
+++ dataPreProcessing/data_pre_temp.py
@@ -0,0 +1,5 @@
+#importing libraries 
+import numpy as np  # includes mathematical tools
+import pandas as pd  # to import and manage datasets
+# import matplotlib.pyplot as plt # to plot nice charts 
+# import matplotlib.pyplot as plt
---
+++ dataPreProcessing/notes.cpp
@@ -0,0 +1,9 @@
+/*
+
+the first columns - independent variables 
+the last columns / result columns - dependent variables 
+
+
+
+
+*/
